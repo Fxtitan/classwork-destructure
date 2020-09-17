8d377431ec2ba181dab0bc59bb31179f47e4da91
Sharod Bailey <Sharod.Bailey@codeimmersives.comy>
2020-09-17 15:25:20 -0400

    done

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 /*You will probably not have to take destructuring to such a level as this but this is for you to really wrap your brains around the mechanics.
 EVERY EXERCISE SHOULD USE DESTRUCTURING*/
 
-
-1. let arr = [
-   ['name', 'jon'],
-   ['age', 20],
-   ['eyes', 'blue'],
-   ];
+/*1.*/ 
+let arr = [
+  ['name', 'jon'],
+  ['age', 20],
+  ['eyes', 'blue'],
+];
  //Create an object out of the arr above then using destructuring, declare variables using the object keys.
- 
- 
-2. 
-let arr = [1,2,3,4,5,6]
+let interesting = Object.fromEntries(arr)
+console.log(interesting)
+
+const { name, age, eyes} = interesting;
+console.log(name)
+console.log(age)
+console.log(eyes)
+console.log(`I am ${name}, I'm ${age} years old and my eyes are ${eyes}.`);
+
+
+// 2. 
+let arr2 = [1,2,3,4,5,6]
   //a. Set 3 variables names: the third index should be called 'three', the fourth item 'four' and the rest of the items thereafter 'rest'
   //b. Swap the third and fourth item variable values
+
+
+let [one, two, three, four, ...rest] = arr2;
+[three,four] = [four,three]
+console.log(three);
+console.log(four);
+
